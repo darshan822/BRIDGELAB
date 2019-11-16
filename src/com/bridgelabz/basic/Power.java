9ee20d7616253c54607b7628cc74e7455322702f
darshan822 <naikdarshan078@gmail.com>
2019-11-16 12:14:12 +0530

    version 2

--- src/com/bridgelabz/basic/Power.java
+++ src/com/bridgelabz/basic/Power.java
@@ -4,7 +4,7 @@ import com.bridgelabz.utility.Utility;
 public class Power {
 
 	public static void main(String[] args) {
-		System.out.println("enter value of n");//to find 2 power of number
+		System.out.println("enter value of n ");//to find 2 power of number
 		try {
 		int n=Utility.readInt();
 		int i=0;
